18ddb1386723a05963a24890a0760cef673d572a
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-06-05 21:42:29 -0400

    switched to vs code, started registering copper ore

--- src/main/java/com/nailuj29gaming/moreminerals/moreminerals/MoreMinerals.java
+++ src/main/java/com/nailuj29gaming/moreminerals/moreminerals/MoreMinerals.java
@@ -44,6 +44,8 @@ public class MoreMinerals implements ModInitializer {
         Registry.register(Registry.BLOCK, new Identifier(MODID, "copper_block"), MoreMineralsBlocks.COPPER_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(MODID, "copper_block"), MoreMineralsItems.COPPER_BLOCK_ITEM);
 
+        Registry.register(Registry.BLOCK, new Identifier(MODID, "copper_ore"), MoreMineralsBlocks.COPPER_BLOCK);
+
         // Tools
         Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_pickaxe"), MoreMineralsItems.RUBY_PICKAXE);
         Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_sword"), MoreMineralsItems.RUBY_SWORD);
